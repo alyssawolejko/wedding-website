69ad2353e822015c0f0a9e2aa1aec9295da89106
Alyssa Wolejko <awolejko@ADKs-MacBook-Pro-3.local>
2020-02-23 17:18:20 -0500

    Updates

---
+++ precache-manifest.0e4610eaf5a44bbfe67b8c352b27b250.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6e8d1bacd1c9e2fcf069aa0411b7711",
+    "url": "/wedding-website/index.html"
+  },
+  {
+    "revision": "318389415410db03e601",
+    "url": "/wedding-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8dc6512b3fa62a8398e0",
+    "url": "/wedding-website/static/js/2.bad77509.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wedding-website/static/js/2.bad77509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318389415410db03e601",
+    "url": "/wedding-website/static/js/main.fd4a54b0.chunk.js"
+  },
+  {
+    "revision": "de85eb90b107f8f8adcb",
+    "url": "/wedding-website/static/js/runtime-main.67162485.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wedding-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
